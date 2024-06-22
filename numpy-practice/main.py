8f5d6f6ddc4d2c0b60a49c343c69010eab079ddd
mikan-919 <131849133+mikan-919@users.noreply.github.com>
2024-06-22 18:20:22 +0900

    minst loader flags added

--- numpy-practice/main.py
+++ numpy-practice/main.py
@@ -1,35 +1,51 @@
 import mnist
 import numpy as np
+
 np.set_printoptions(linewidth=300)
 
+
 def sigmoid(x):
-  return 1/(1+np.exp(-x))
+    return 1 / (1 + np.exp(-x))
+
+
 def identity_function(x):
-  return x
+    return x
+
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    return exp_a / sum_exp_a
+
 
 def init_network():
-  network={}
-  network['W1']=np.array([[0.1,0.3,0.5],[0.2,0.4,0.6]])
-  network['W2']=np.array([[0.1,0.4],[0.2,0.5],[0.3,0.6]])
-  network['W3']=np.array([[0.1,0.3],[0.2,0.4]])
-  network['b1']=np.array([0.1,0.2,0.3])
-  network['b2']=np.array([0.1,0.2])
-  network['b3']=np.array([0.1,0.2])
-  return network
+    network = {}
+    network["W1"] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network["W2"] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    network["W3"] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network["b1"] = np.array([0.1, 0.2, 0.3])
+    network["b2"] = np.array([0.1, 0.2])
+    network["b3"] = np.array([0.1, 0.2])
+    return network
 
 
-def forward(network,x):
-  a1=np.dot(x,network["W1"])+network["b1"]
-  z1=sigmoid(a1)
-  a2=np.dot(z1,network["W2"])+network["b2"]
-  z2=sigmoid(a2)
-  a3=np.dot(z2,network["W3"])+network["b3"]
+def forward(network, x):
+    a1 = np.dot(x, network["W1"]) + network["b1"]
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, network["W2"]) + network["b2"]
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, network["W3"]) + network["b3"]
 
-  y=identity_function(a3)
+    y = identity_function(a3)
 
-  return y
+    return y
 
-network=init_network()
-x=np.array([1.0,0.5])
-y=forward(network,x)
+
+network = init_network()
+x = np.array([1.0, 0.5])
+y = forward(network, x)
 print(y)
+dataset = mnist.load_mnist(one_hot=True)
+print(dataset[0][1][1])
+print(dataset[0][1][0])
